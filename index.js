02c72abedb28dd63006762ebef6aad8d473e6d5a
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-03-21 18:39:39 -0400

    commit tree 76b0a4ada41b70ff7b2ef5a62f06429a864a683a

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random()*1) + 1
+
+const num3 = 28
+const num4 = 6
+const mod = num3 % num4
+
+const max = Math.max(1, 3, 7, 5, 20)
